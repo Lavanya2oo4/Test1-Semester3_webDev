127ca9cfc22645c15d054c84f5c962e206fb95b3
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-16 13:32:30 -0500

    Added functionality to addData button

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ noActivity = 4;
 totalWeight = 100;
 ERR = document.getElementById("ERR");
 
+
+//code to add  no.,activity and weight  to table when add activity button clicked
 function addActivity() {
-  window.alert("addActivity");
+    let rowCount=document.getElementsByTagName("tr").length
+    let table=document.getElementById("theTable")
+    
+    let newRow=table.insertRow(-1)
+
+    let c1=newRow.insertCell(0).innerHTML=rowCount
+    let c2=newRow.insertCell(1).innerHTML=document.getElementById("activity").value
+    let c3=newRow.insertCell(2).innerHTML=document.getElementById("weight").value
 }
 
  
