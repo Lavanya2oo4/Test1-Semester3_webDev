fd47b7c8b08e9351f7a350992c7fb7a6ee5a19af
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-16 14:24:37 -0500

    Adding code to validate grade and activityNo and Adding and updating grade based on activity number

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ function addActivity() {
     }
 
     let weightValue=document.getElementById("weight").value
+    
 
     //validating weight field
-    if(weightValue=="" || weightValue>100 ||weightValue<1){
+    if(weightValue=="" || weightValue>100 ||weightValue<1  ){
         showError("Weight is rejected❌")
         return
     }
@@ -25,13 +26,13 @@ function addActivity() {
     let table=document.getElementById("theTable")
     
     let newRow=table.insertRow(-1)
+    newRow.id=rowCount
 
     let c1=newRow.insertCell(0).innerHTML=rowCount
     let c2=newRow.insertCell(1).innerHTML=activityValue
     let c3=newRow.insertCell(2).innerHTML=weightValue
     let c4=newRow.insertCell(3).innerHTML=""
-
-
+    
     //making fields blank after adding activity
     document.getElementById("activity").value=""
     document.getElementById("weight").value=""
@@ -42,8 +43,30 @@ function deleteActivity() {
   window.alert("deleteActivity");
 }
 
+
+
+//adding grade to given activity number
 function addGrade() {
-  window.alert("addGrade");
+    let rowCount=document.getElementsByTagName("tr").length
+
+    let grade=document.getElementById("grade").value
+    let activityNo=document.getElementById("activityNo").value
+
+    //validating grade
+    if(grade=="" || grade>100||grade<0){
+        showError("Grade is Rejected")
+        return
+    }
+    //validating activityNo
+    if(activityNo>rowCount || activityNo<=0){
+        showError("Activity No is rejected")
+        
+        return
+    }
+   let row= document.getElementById(activityNo).getElementsByTagName("td")[3]
+   row.innerHTML=grade
+
+
 }  
 
 
