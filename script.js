b4b15940acb8ff62226c005c694de7d7efe6e0ea
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-16 13:18:17 -0500

    Downloaded the file and created basic syntax

---
+++ script.js
@@ -0,0 +1,16 @@
+noActivity = 4;
+totalWeight = 100;
+ERR = document.getElementById("ERR");
+
+function addActivity() {
+  window.alert("addActivity");
+}
+
+ 
+function deleteActivity() {
+  window.alert("deleteActivity");
+}
+
+function addGrade() {
+  window.alert("addGrade");
+}  
